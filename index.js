e9c21808d5c850410320e07339d031dba5f8ac57
Greg Ward <codenlighten1@gmail.com>
2021-07-21 21:27:29 -0400

    procfile deployed

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/bsvhunter.html');
+  res.sendFile(__dirname + '/bsvhunter.html');
 });
 app.get('/index.html', function (req, res) {
-  res.sendfile(__dirname + '/bsvhunter.html');
+  res.sendFile(__dirname + '/bsvhunter.html');
 });
 app.get('/:url', function (req, res) {  
-  res.sendfile(__dirname + req.url);
+  res.sendFile(__dirname + req.url);
 });
 app.get('/:url[.js]', function (req, res) {
-  res.sendfile(__dirname + req.url);
+  res.sendFile(__dirname + req.url);
 });
 app.get('/:url[.css]', function (req, res) {    
-  res.sendfile(__dirname + req.url);
+  res.sendFile(__dirname + req.url);
 });
 app.get('/assets/:url[.png]', function (req, res) {
-  res.sendfile(__dirname + req.url);
+  res.sendFile(__dirname + req.url);
 });
 
 http.listen(process.env.PORT || 3000, function(){
