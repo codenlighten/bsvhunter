4f4a3e31221885a952e64942ed5060521a4e1d47
Greg Ward <codenlighten1@gmail.com>
2021-07-21 21:08:49 -0400

    first deploy

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/bsvhunter.html');
+});
+app.get('/index.html', function (req, res) {
+  res.sendfile(__dirname + '/bsvhunter.html');
+});
+app.get('/:url', function (req, res) {  
+  res.sendfile(__dirname + req.url);
+});
+app.get('/:url[.js]', function (req, res) {
+  res.sendfile(__dirname + req.url);
+});
+app.get('/:url[.css]', function (req, res) {    
+  res.sendfile(__dirname + req.url);
+});
+app.get('/assets/:url[.png]', function (req, res) {
+  res.sendfile(__dirname + req.url);
+});
+
+http.listen(3000, function(){
+    console.log("Listening on port 3000");
+})
